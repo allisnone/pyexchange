8cf187be2ed97e8afb4b3495f031c438c8b2e957
allisnone <zgx20022002@163.com>
2020-04-26 22:36:34 +0800

    update

--- skyexchange.py
+++ skyexchange.py
@@ -3,13 +3,24 @@ from exchangelib import Credentials, Account,Configuration,DELEGATE
 #172.22.3.166    mail.skyguardex.com
 email= 'zhoubaodong@skyguardex.com'
 pwd = 'Firewall1'
+
+#* PrimarySMTPAddress
+#* WINDOMAIN\\username
+#* User Principal Name (UPN)
 credentials = Credentials(email, pwd)
 
 config = Configuration(server='mail.skyguardex.com', credentials=credentials)
+#config = Configuration(service_endpoint='https://mail.example.com/EWS/Exchange.asmx', credentials=credentials)
 
 account = Account(primary_smtp_address=email, config=config,
                   autodiscover=False, access_type=DELEGATE)
 
+"""
+version = Version(build=Build(15, 0, 12, 34))
+config = Configuration(
+    server='example.com', credentials=credentials, version=version, auth_type=NTLM
+)
+"""
 #account = Account(email, credentials=credentials, autodiscover=True)
 
 for item in account.inbox.all().order_by('-datetime_received')[:100]:
